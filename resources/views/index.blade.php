d3284226c5def5cc88e09a8910b2eb8635fd207c
Prem Prakash <iamprem021@gmail.com>
2023-06-15 16:49:53 +0530

    done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,10 @@
 <head>  
    <title> CRUD Operation in Laravel </title>  
    <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">  
+   <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+   <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+ 
 </head>  
 <body>  
   
@@ -13,13 +17,16 @@
   <br>
   <br>
   
-<table class="table table-borderd">  
-   <tr>  
-      <th>Name</th>  
-      <th>Email</th>  
-      <th>City</th>  
-      <th>Action</th>  
-   </tr>  
+<table id="data" style="width:100%" class="table display table-borderd">  
+   <thead>
+      <tr>  
+         <th>Name</th>  
+         <th>Email</th>  
+         <th>City</th>  
+         <th>Action</th>  
+      </tr>  
+   </thead>
+   <tbody>
    @foreach($mydata as $udata)
    <tr>
       <td>{{$udata['name']}}</td>
@@ -32,9 +39,16 @@
       </td>
    </tr>
    @endforeach
+</tbody>
      
 </table>  
 </div>  
-  
+  <script>
+      $(document).ready(function () {
+    $('#data').DataTable({
+        order: [[3, 'desc']],
+    });
+});
+  </script>
 </body>  
 </html>  
